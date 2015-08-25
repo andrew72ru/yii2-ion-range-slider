c23b89c50abec9368098966345d0ba9265719c2e
Андрей Ждановских <andrew72ru@gmail.com>
2015-08-25 17:11:23 +0500

    Only base css, skins in separated files

--- SliderAsset.php
+++ SliderAsset.php
@@ -8,8 +8,15 @@
 
 namespace andrew72ru\ionrange;
 
+use Yii;
+use yii\web\AssetBundle;
 
-class SliderAsset
+class SliderAsset extends AssetBundle
 {
-
+    public $sourcePath = '@bower/ionrangeslider';
+    public $js = ['js/ion.rangeSlider.min.js'];
+    public $css = [
+        'css/ion.rangeSlider.css',
+    ];
+    public $depends = ['yii\web\JqueryAsset'];
 }
